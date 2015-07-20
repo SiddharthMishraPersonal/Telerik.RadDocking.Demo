473a22a425e687009b6521fdea7854c25f06bf36
Siddharth Mishra <siddharth82mishra@gmail.com>
2015-07-20 11:59:37 -0600

    Initial Checkin
    Created Visual Studio Project with Telerik RadDocking control's
    libraries.

---
+++ Telerik.RadDocking.Demo/Telerik.RadDocking.Demo/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace Telerik.RadDocking.Demo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+           this.InitializeComponent();
+
+        }
+
+    }
+}
